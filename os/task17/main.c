8b8cd4dbbc1e40a3dc31e3cfc27b62ed1b0c8903
Bogdan Lukin <2ZeroSix1@gmail.com>
2017-03-24 01:20:57 +0700

    os.task17 complete

--- os/task17/main.c
+++ os/task17/main.c
@@ -10,7 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-typedef int (*handler)(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen);
+
+typedef int (*handler)(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth);
 typedef struct termios termios;
 
 termios* setTerminalAttr(int terminal) {
@@ -25,7 +26,7 @@ termios* setTerminalAttr(int terminal) {
     return newAttr;
 }
 
-int endOfRead(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
+int endOfRead(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
     if (*curStr == 0) {
         return 1;
     } else {
@@ -33,103 +34,85 @@ int endOfRead(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_
     }
 }
 
-int eraseLine(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
-    if (write(terminal, "\r", strlen("\r")) == -1) {
-        return -1;
-    }
-    for (size_t i = 0; i < maxStrLen; ++i) {
-        if (!*curStr) break;
-        for (size_t j = 0; j < wcwidth(curStr[i]); ++j) {
-            if (write(terminal, " ", strlen(" ")) == -1) {
-                return -1;
-            }
-        }
-    }
-    if (write(terminal, "\r", strlen("\r")) == -1) {
-        return -1;
-    }
-    memset(curStr, 0, (maxStrLen + 1)*sizeof(wchar_t));
+int eraseLine(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
+    if (write(terminal, "\r", strlen("\r")) == -1) return -1;
+    for (size_t i = 0; i < *strWidth; ++i)
+        if (write(terminal, " ", strlen(" ")) == -1) return -1;
+    if (write(terminal, "\r", strlen("\r")) == -1) return -1;
+    wmemset(curStr, L'\0', *strWidth);
+    *strWidth = 0;
     return 0;
 }
-int eraseWord(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
+
+int eraseWord(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
     size_t len = wcslen(curStr);
     size_t wordLen;
-    for (wordLen = 0; wordLen < len; ++wordLen) {
-        if (!iswspace(curStr[len - wordLen - 1])) {
-            break;
-        }
-    }
-    for (; wordLen < len; ++wordLen) {
-        if (iswspace(curStr[len - wordLen - 1])) {
-            break;
-        }
-    }
+    for (wordLen = 0; wordLen < len; ++wordLen)
+        if (!iswspace(curStr[len - wordLen - 1])) break;
+    for (; wordLen < len; ++wordLen)
+        if (iswspace(curStr[len - wordLen - 1])) break;
     for (size_t i = 0; i < wordLen; ++i) {
-        for (int j = 0; j < wcwidth(curStr[len - i - 1]); ++j) {
-            if (write(terminal, "\b \b", strlen("\b \b")) == -1) {
-                return -1;
-            }
-        }
+        for (int j = 0; j < wcwidth(curStr[len - i - 1]); ++j) 
+            if (write(terminal, "\b \b", strlen("\b \b")) == -1) return -1;
+        *strWidth -= wcwidth(curStr[len - i - 1]);
         curStr[len - i - 1] = 0;
     }
     return 0;
 }
 
-int eraseWchar(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
+int eraseWchar(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
     size_t len = wcslen(curStr);
-    for (int i = 0; i < wcwidth(curStr[len ? len - 1 : len]); ++i) {
-        if (write(terminal, "\b \b", strlen("\b \b")) == -1) {
-            return -1;
-        }
-    }
+    for (int i = 0; i < wcwidth(curStr[len ? len - 1 : len]); ++i)
+        if (write(terminal, "\b \b", strlen("\b \b")) == -1) return -1;
+    *strWidth -= wcwidth(curStr[len ? len - 1 : 0]);
     curStr[len ? len - 1 : 0] = 0;
     return 0;
 }
 
-
-int printNewLine(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
-    if (write(terminal, "\n", strlen("\n")) == -1) {
-        return -1;
-    }
-    memset(curStr, 0, (maxStrLen + 1)*sizeof(wchar_t));
+int printNewLine(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
+    if (write(terminal, "\n", strlen("\n")) == -1) return -1;
+    wmemset(curStr, 0, *strWidth);
+    *strWidth = 0;
     return 0;
 }
 
-int printHorn(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
-    if (write(terminal, "\007", strlen("\007")) == -1) {
-        return -1;
-    }
+int printHorn(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
+    if (write(terminal, "\007", strlen("\007")) == -1) return -1;
     return 0;
 }
 
-int printWchar(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrLen) {
+int printWchar(int terminal, wchar_t wc, wchar_t* curStr, size_t* strWidth, size_t maxStrWidth) {
     size_t len = wcslen(curStr);
-    if (*strWidth + wcwidth(wc) > maxStrLen) {
-        size_t lastWordLen;
-        for (lastWordLen = 0; lastWordLen < len; ++lastWordLen) {
-            if (iswspace(curStr[len - lastWordLen - 1])) break;
-        }
-        size_t lastWordWidth = wcswidth(curStr + len - lastWordLen - 1, lastWordLen);
-        if (lastWordWidth < maxStrLen) {
-            for (size_t i = len - 1; i > len - lastWordLen - 1; --i) {
-                for (int j = 0; j < wcwidth(curStr[len - i - 1]); ++j) {
-                    if (write(terminal, "\b \b", strlen("\b \b")) == -1) {
-                        return -1;
-                    }
-                }
-            }
-            wcsncpy(curStr, curStr + len - lastWordLen - 1, lastWordLen);
-            wmemset(curStr + lastWordLen, 0, len - lastWordLen);
-            printNewLine(terminal, wc, curStr, strWidth, maxStrLen);
-            for (size_t i = 0; i < lastWordLen; ++i) {
+    if (*strWidth + wcwidth(wc) > maxStrWidth) {
 
-            }
+        size_t lastWordLen = 0;
+        if (!iswspace(wc))
+            for (lastWordLen = 0; lastWordLen < len; ++lastWordLen)
+                if (iswspace(curStr[len - 1 - lastWordLen])) break;
+        size_t lastWordWidth = wcswidth(curStr + len - lastWordLen, lastWordLen);
+
+        if (lastWordWidth < maxStrWidth) {
+
+            for (size_t i = 0; i < lastWordWidth; ++i)
+                if (write(terminal, "\b \b", strlen("\b \b")) == -1) return -1;
+            
+            for (size_t i = 0; i < lastWordLen; ++i) curStr[i] = curStr[i + len - lastWordLen];
+            wmemset(curStr + lastWordLen, L'\0', maxStrWidth - lastWordLen);
+
+            *strWidth = 0;
+            if (printNewLine(terminal, wc, curStr, strWidth, maxStrWidth) == -1) return -1;
+            
+            *strWidth = len = lastWordWidth;
+            if (write(terminal, curStr, len*sizeof(wchar_t)) == -1) return -1;
+
+        } else {
+            if (printNewLine(terminal, wc, curStr, strWidth, maxStrWidth) == -1) return -1;
+            len = 0;
         }
-        len = 0;
     }
-    if (write(terminal, &wc, sizeof(wchar_t)) == -1) {
-        return -1;
-    }
+    
+    if (write(terminal, &wc, sizeof(wchar_t)) == -1) return -1;
+    
     curStr[len] = wc;
     *strWidth += wcwidth(wc);
     return 0;
@@ -146,9 +129,10 @@ handler getHandler(wint_t wc,  termios* attributes) {
     else                                            return printWchar;
 }
 
-int editor(int terminal, size_t maxStrLen, const char* locale) {
+int editor(int terminal, size_t maxStrWidth, const char* locale) {
     setlocale(LC_ALL, locale ? locale : "");
     termios oldAttr, *attr;
+
     if (tcgetattr(terminal, &oldAttr) == -1) {
         perror("Error: can't save old configuration");
         return -1;
@@ -157,27 +141,29 @@ int editor(int terminal, size_t maxStrLen, const char* locale) {
         perror("Error: can't set terminal configuration");
         return -1;
     }
+
     wchar_t wc = 0;
-    wchar_t *curStr = (wchar_t*)calloc(maxStrLen + 1, sizeof(wchar_t));
+    wchar_t *curStr = (wchar_t*)calloc(maxStrWidth + 1, sizeof(wchar_t));
     size_t strWidth = 0;
+
     // main loop
     while (read(terminal, &wc, sizeof(wchar_t)) > 0) {
-        // printf("%u\n", wc);
-        int handleRet = getHandler(wc, attr)(terminal, wc, curStr, &strWidth, maxStrLen);
+        int handleRet = getHandler(wc, attr)(terminal, wc, curStr, &strWidth, maxStrWidth);
+
         if(handleRet == -1) {
+
             perror("Error in handler");
             free(attr);
-            if (tcflush(terminal, TCIFLUSH) == -1) {
+            if (tcflush(terminal, TCIFLUSH) == -1)
                 perror("Error: can't flush terminal");
-                return -1;
-            }
-            if (tcsetattr(terminal, TCSANOW, &oldAttr) == -1) {
+            if (tcsetattr(terminal, TCSANOW, &oldAttr) == -1)
                 perror("Error: can't reset attributes");
-                return -1;
-            }
+            return -1;
+
         } else if (handleRet == 1) {
             break;
         }
+
         wc = 0;
     }
 
@@ -191,6 +177,7 @@ int editor(int terminal, size_t maxStrLen, const char* locale) {
         free(attr);
         return -1;
     }
+
     return 0;
 }
 
