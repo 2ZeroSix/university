93bcd5ca6e3668673d05365a7408f1d442f85053
Bogdan Lukin <2ZeroSix1@gmail.com>
2017-04-07 14:59:14 +0700

    os.task21 complete; os.task22 almost complete

--- os/task21/main.c
+++ os/task21/main.c
@@ -6,11 +6,10 @@
 #include <stdlib.h>
 #include <wchar.h>
 
-typedef void (*sighandler_t)(int);
-// typedef struct sigaction sigaction;
+#define DEL_SEQUENCE 2117294875
 
 static struct termios stored_settings;
-size_t escapeCouter = 0;
+size_t deleteCouter = 0;
 
 
 int setByKeyInput(void) {
@@ -20,7 +19,7 @@ int setByKeyInput(void) {
     }
     new_settings            = stored_settings;
     new_settings.c_lflag    &= ~(ICANON | ECHO);
-    new_settings.c_lflag    |= ISIG;
+    // new_settings.c_lflag    |= ISIG;
 
     new_settings.c_cc[VTIME]= 0;
     new_settings.c_cc[VMIN] = 1;
@@ -37,14 +36,14 @@ int resetByKeyInput(void) {
     return 0;
 }
 
-void finalize() {
+void finalize(int retVal) {
     resetByKeyInput();
+    exit(retVal);
 }
 
 void onQuit(int param) {
-    printf("Count of delete button presses: %zu\n", escapeCouter);
-    finalize();
-    exit(0);
+    printf("Count of delete button presses: %zu\n", deleteCouter);
+    finalize(0);
 }
 
 int setQuitAction() {
@@ -61,10 +60,11 @@ int main(int argc, char** argv) {
     }
     wint_t wc;
     while(read(0, &wc, sizeof(wc)) > 0) {
-        // printf("%u\n", wc);
-        if (wc == 2117294875) {
-            putchar('\07');
-            ++escapeCouter;
+        if (wc == DEL_SEQUENCE) {
+            putchar('\007');
+            fflush(stdout);
+            ++deleteCouter;
         }
     }
+    finalize(1);
 }
---
+++ os/task22/main.c
@@ -0,0 +1,87 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MPR_BUF_SIZE 256 // size of buffer for every file in multiplexRead
+
+int waitForInput(struct timeval tv) {
+    fd_set fd_s;
+    FD_ZERO(&fd_s);
+    FD_SET(0, &fd_s);
+    switch (select(1, &fd_s, NULL, NULL, &tv)) {
+        case -1:    return -1;
+        case  0:    return  1;
+        default:    return  0;
+    }
+}
+
+int multiplexRead(int fileCount, char** filenames) {
+    int* files = (int*)calloc(fileCount, sizeof(int));
+    if (files == NULL) {
+        perror("can't allocate array of file descriptors");
+        return -1;
+    }
+
+    for (int i = 0; i < fileCount; ++i) {
+        files[i] = open(filenames[i], O_RDONLY);
+        if (files[i] == -1) {
+            fprintf(stderr, "Can't open file. ");
+            perror(filenames[i]);
+            --i;
+            --fileCount;
+            errno = 0;
+        }
+    }
+
+    size_t* bufOffset = (size_t*)calloc(fileCount, sizeof(size_t));
+    char** buffers = (char**)calloc(fileCount, sizeof(char*));
+    for (int i = 0; i < fileCount; ++i) 
+        buffers[i] = (char*) calloc(MPR_BUF_SIZE, sizeof(char));
+
+    int check = 1;
+    while (check) {
+        check = 0;
+        for (int i = 0; i < fileCount; ++i) {
+            usleep(100000);
+            if(files[i] == -1) continue;
+            check = 1;
+            if (bufOffset[i] == 0) {
+                int retval = read(files[i], buffers[i], MPR_BUF_SIZE - 1);
+                if (retval <= 0) {
+                    close(files[i]);
+                    files[i] = -1;
+                    free(buffers[i]);
+                    continue;
+                }
+                buffers[retval] = 0;
+            }
+
+            char* curPos = buffers[i] + bufOffset[i];
+            char* newLinePos = strchr(curPos, '\n');
+            if (newLinePos == NULL) {
+                size_t len = strlen(curPos);
+                write(1, curPos, len);
+                bufOffset[i] = 0;
+                --i;
+                continue;
+            } else {
+                size_t len = (size_t)(newLinePos - (buffers[i] + bufOffset[i]) + 1);
+                write(1, curPos, len);
+                bufOffset[i] += len;
+                bufOffset[i] %= MPR_BUF_SIZE - 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    return multiplexRead(argc - 1, argv + 1) ? 1 : 0;
+}
