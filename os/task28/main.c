6ee870ef718b2f165302932e5613e79d5599ba6f
Lukin Bogdan Aleksandrovich <b.lukin@g.nsu.ru>
2017-04-20 00:30:26 +0700

    os.task28 complete

--- os/task28/main.c
+++ os/task28/main.c
@@ -1,14 +1,23 @@
 #include <libgen.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 
 int main() {
     srand(time(NULL));
     FILE* fd[2];
-    p2open("sort", fd);
+    p2open("sort -n", fd);
     for (int i = 0; i < 100; ++i) {
-        fprintf(fd[1], "%d\n", rand()%100);
+        fprintf(fd[0], "%d\n", rand()%100);
+    }
+    fclose(fd[0]);
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            int num;
+            fscanf(fd[1], "%d", &num);
+            fprintf(stdout, "%4d ", num);
+        }
+        fputs("\n", stdout);
     }
     p2close(fd);
     return 0;
